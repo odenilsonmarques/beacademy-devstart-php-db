58764c3424b4b81eb58bd91ac74061245774688f
Odenilson Marques <odenilson.marques@gmz.solutions>
2022-06-18 16:11:25 -0300

    importacao do banco de dados

--- projeto-final/db/db_story.sql
+++ projeto-final/db/db_story.sql
@@ -1,33 +1,82 @@
-create database db_story;
+-- MySQL dump 10.13  Distrib 8.0.29, for Linux (x86_64)
+--
+-- Host: localhost    Database: db_story
+-- ------------------------------------------------------
+-- Server version	8.0.29-0ubuntu0.20.04.3
 
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-use db_story;
+--
+-- Table structure for table `tb_category`
+--
 
-create table tb_category(
-    id int(11) not null primary key auto_increment, 
-    name varchar(45) not null, 
-    description varchar (100) not null
-);
+DROP TABLE IF EXISTS `tb_category`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tb_category` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  `description` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-insert into tb_category(name, description)
-values
-('Informatica', 'Produto de informatica e acessorios para computador'),
-('Escritorio', 'Canetas, Cardenos, folhas'),
-('Eletronicos', 'Tvs, Som portatil, caixa de som');
+--
+-- Dumping data for table `tb_category`
+--
 
+LOCK TABLES `tb_category` WRITE;
+/*!40000 ALTER TABLE `tb_category` DISABLE KEYS */;
+INSERT INTO `tb_category` VALUES (14,'Informática','Mouse para computador'),(15,'Escritorio','moveis para escritorio'),(16,'Eletronico','dispositivos em geral');
+/*!40000 ALTER TABLE `tb_category` ENABLE KEYS */;
+UNLOCK TABLES;
 
-create table tb_product(
-    id int(11) not null primary key auto_increment, 
-    name varchar(45) not null, 
-    description varchar (100) not null,
-    photo varchar(255) not null,
-    value float(5,2) not null,
-    quantity int(5) not null,
-    create_at datetime not null
-);
+--
+-- Table structure for table `tb_product`
+--
 
-insert into tb_product(name, description, photo, value, quantity, create_at)
-values
-('Notebook', 'notebook dell inspirion i5','https://www.dell.com/pt-br/shop/notebooks/notebook-inspiron-13/spd/inspiron-13-5310-laptop/i5310w1002w', 5.599,00, 10),
+DROP TABLE IF EXISTS `tb_product`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tb_product` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) DEFAULT NULL,
+  `description` varchar(100) DEFAULT NULL,
+  `photo` varchar(255) DEFAULT NULL,
+  `value` decimal(14,2) DEFAULT NULL,
+  `quantity` int DEFAULT NULL,
+  `create_at` datetime DEFAULT NULL,
+  `category_id` int NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-('Mesa de escritorio', 'Mesa de Escritório Studio Preta 150 cm', 'https://www.escritolandia.com.br/#mz-expanded-view-1153817935847', 234,97, 8)
+--
+-- Dumping data for table `tb_product`
+--
+
+LOCK TABLES `tb_product` WRITE;
+/*!40000 ALTER TABLE `tb_product` DISABLE KEYS */;
+INSERT INTO `tb_product` VALUES (6,'Notebook','notebook i5, 250 sd','https://a-static.mlcdn.com.br/800x560/notebook-dell-inspiron-3000-3501-a46p-intel-core-i5-8gb-256gb-ssd-156-windows-10/magazineluiza/232441100/4018ac4f263d295ab5f78a42d6d134e1.jpg',5.00,3,'2022-06-17 23:20:06',14),(7,'Notebook','Notebook i3, hd 500','https://m.media-amazon.com/images/I/61kZqvX5YLL._AC_SY355_.jpg',3.00,10,'2022-06-17 23:20:59',14),(8,'mouse','mouse ergometrico','https://img.olx.com.br/thumbs256x256/65/651165556848871.jpg',100.00,6,'2022-06-17 23:22:32',14),(9,'celular','celular motog t500','https://a-static.mlcdn.com.br/280x210/smartphone-samsung-galaxy-a32-128gb-violeta-4g-4gb-ram-tela-64-cam-quadrupla-selfie-20mp/magazineluiza/155624100/1595344320db4b74ffa559fcb01a992c.jpg',2.00,4,'2022-06-17 23:24:06',16);
+/*!40000 ALTER TABLE `tb_product` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-06-18 16:05:54
